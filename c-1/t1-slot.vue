ff1e01c05e7f23906ce6a4067f61986f072eacae
jializhong <jializhong@supcon.com>
2023-03-22 16:53:04 +0800

    vue cli

---
+++ c-1/t1-slot.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+<script src="../lib/vue.js"></script>
---
+++ vue-cli.md
@@ -0,0 +1,5 @@
+对于vue-cli来说，
+ npm uninstall -g vue-cli 最高只能安装2.x cli版本  创建项目使用
+ vue webpack [项目名]，webpack是选择项目骨架，使用webpack打包来兼容浏览器
+ 
+ 如果想使用vue create [项目名]，需要使用Vue CLI 3版本，使用npm install -g @vue/cli
